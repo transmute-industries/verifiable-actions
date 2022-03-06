801a4fd268691ee82cc2eff96585470334987c74
Orie Steele <orie@transmute.industries>
2022-03-06 13:36:03 -0600

    chore: refactor cli interface

--- src/resources/did/generate.js
+++ src/resources/did/generate.js
@@ -1,23 +1,16 @@
-const fs = require("fs");
 const cli = require("@transmute/cli");
 
 const generate = async (inputs) => {
   let outputs = {};
   let { username, repository, mnemonic, hdPath, keyType } = inputs;
-  const data = await cli.commands.did.createDocument({
+  await cli.commands.did.createDocumentHandler({
     username,
     repository,
     mnemonic,
     hdpath: hdPath,
     type: keyType,
+    output: inputs.fileOutput,
   });
-
-  outputs = data;
-
-  if (inputs.fileOutput) {
-    fs.writeFileSync(inputs.fileOutput, JSON.stringify(outputs, null, 2));
-  }
-
   return outputs;
 };
 
